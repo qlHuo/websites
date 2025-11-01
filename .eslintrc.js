e0264012be432bed0cf0b6c6bacf88618a935c79
holyer <1322346367@qq.com>
2025-11-01 15:22:02 +0800

    update: 增加一些网址

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,7 +12,7 @@ module.exports = {
     parser: '@babel/eslint-parser'
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'no-console': 'warn',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
     'vue/multi-word-component-names': 'off',
     'vue/no-multiple-template-root': 0,
--- src/constant/website.js
+++ src/constant/website.js
@@ -33,13 +33,7 @@ export default [
         desc: '中国最大的开发者社区，提供技术文章和资源分享。',
         icon: 'https://www.csdn.net/favicon.ico'
       },
-      {
-        id: 15,
-        title: '掘金',
-        url: 'https://juejin.cn',
-        desc: '面向开发者的技术社区，分享前沿技术和经验。',
-        icon: 'https://lf-web-assets.juejin.cn/obj/juejin-web/xitu_juejin_web/static/favicons/favicon-32x32.png'
-      },
+
       {
         id: 16,
         title: 'V2EX',
@@ -66,7 +60,7 @@ export default [
         title: '开源中国',
         url: 'https://www.oschina.net',
         desc: '中国领先的开源技术社区，提供开源项目和资讯。',
-        icon: 'https://static.oschina.net/uploads/img/201707/07165901_6o3n.png'
+        icon: 'https://static.oschina.net/new-osc/img/favicon.ico'
       }
     ]
   },
@@ -78,45 +72,38 @@ export default [
     children: [
       {
         id: 21,
-        title: 'DeepSeek',
-        url: 'https://deepseek.ai', // AI 代码搜索引擎
-        desc: '基于 AI 的代码搜索引擎，帮助开发者快速找到所需代码片段。',
-        icon: 'https://deepseek.ai/favicon.ico'
-      },
-      {
-        id: 22,
-        title: '问小白',
-        url: 'https://www.wenxiaobai.com/', // AI 问答平台
-        desc: '基于 AI 的问答平台，帮助用户快速找到所需信息。',
-        icon: 'https://wy-static.wenxiaobai.com/wenxiaobai-web/production/3.15.5/_next/static/media/new_favicon.6d31cfe4.png'
-      },
-      {
-        id: 22,
-        title: '文心一言',
-        url: 'https://yiyan.baidu.com/', // 百度的对话式 AI 模型
-        desc: '百度的对话式 AI 模型，能够进行自然语言交流和回答问题。',
-        icon: 'https://eb-static.cdn.bcebos.com/logo/favicon.ico'
-      },
-      {
-        id: 23,
         title: '豆包',
         url: 'https://www.doubao.com/', // 豆包 AI 工具
         desc: '集成多种 AI 工具的平台，提供文本生成、图像生成等功能。',
         icon: 'https://lf-flow-web-cdn.doubao.com/obj/flow-doubao/doubao/chat/static/image/logo-icon-white-bg.72df0b1a.png'
       },
       {
+        id: 22,
+        title: 'DeepSeek',
+        url: 'https://www.deepseek.com/', // AI 代码搜索引擎
+        desc: '基于 AI 的代码搜索引擎，帮助开发者快速找到所需代码片段。',
+        icon: 'https://www.deepseek.com/favicon.ico'
+      },
+      {
+        id: 23,
+        title: '问小白',
+        url: 'https://www.wenxiaobai.com/', // AI 问答平台
+        desc: '基于 AI 的问答平台，帮助用户快速找到所需信息。',
+        icon: 'https://wy-static.wenxiaobai.com/wenxiaobai-web/production/3.15.5/_next/static/media/new_favicon.6d31cfe4.png'
+      },
+      {
         id: 24,
-        title: 'ChatGPT',
-        url: 'https://chat.openai.com/', // OpenAI 的对话式 AI 模型
-        desc: 'OpenAI 的对话式 AI 模型，能够进行自然语言交流和回答问题。',
-        icon: 'https://chat.openai.com/favicon.ico'
+        title: '腾讯元宝',
+        url: 'https://yuanbao.tencent.com/', // 小米的小爱同学 AI 助手
+        desc: '腾讯元宝,支持文字、图片、文档输入进行搜索. 搜索结果深度打通腾讯旗下产品',
+        icon: 'https://static.yuanbao.tencent.com/m/yuanbao-web/favicon@32.png'
       },
       {
         id: 25,
-        title: 'MidJourney',
-        url: 'https://www.midjourney.com/', // AI 图像生成工具
-        desc: '基于 AI 的图像生成工具，能够根据文本描述生成高质量的图像。',
-        icon: 'https://www.midjourney.com/favicon.ico'
+        title: '文心一言',
+        url: 'https://yiyan.baidu.com/', // 百度的对话式 AI 模型
+        desc: '百度的对话式 AI 模型，能够进行自然语言交流和回答问题。',
+        icon: 'https://eb-static.cdn.bcebos.com/logo/favicon.ico'
       },
       {
         id: 26,
@@ -127,10 +114,17 @@ export default [
       },
       {
         id: 27,
-        title: '文心千帆',
-        url: 'https://qianfan.baidu.com/', // 百度的生成式 AI 平台
-        desc: '百度的生成式 AI 平台，提供多种生成式 AI 模型和服务。',
-        icon: 'https://qianfan.baidu.com/favicon.ico'
+        title: 'MidJourney',
+        url: 'https://www.midjourney.com/', // AI 图像生成工具
+        desc: '基于 AI 的图像生成工具，能够根据文本描述生成高质量的图像。',
+        icon: 'https://www.midjourney.com/favicon.ico'
+      },
+      {
+        id: 28,
+        title: 'ChatGPT',
+        url: 'https://chat.openai.com/', // OpenAI 的对话式 AI 模型
+        desc: 'OpenAI 的对话式 AI 模型，能够进行自然语言交流和回答问题。',
+        icon: 'https://chat.openai.com/favicon.ico'
       }
     ]
   },
@@ -142,73 +136,101 @@ export default [
     children: [
       {
         id: 31,
-        title: 'MDN Web Docs',
-        url: 'https://developer.mozilla.org/', // 前端开发资源
-        desc: '由 Mozilla 维护的前端开发资源，涵盖 HTML、CSS 和 JavaScript 等内容。',
-        icon: 'https://developer.mozilla.org/favicon-48x48.cbbd161b.png'
-      },
-      {
-        id: 32,
-        title: 'TDesign UI',
-        url: 'https://tdesign.tencent.com/', // 腾讯开源的多端 UI 组件库
-        desc: '腾讯开源的多端 UI 组件库，提供丰富的组件和设计资源。',
-        icon: 'https://tdesign.tencent.com/favicon.ico'
-      },
-      {
-        id: 33,
-        title: 'Ant Design',
-        url: 'https://ant.design/', // 蚂蚁金服开源的企业级 UI 设计语言和 React 组件库
-        desc: '蚂蚁金服开源的企业级 UI 设计语言和 React 组件库，广泛应用于中后台产品。',
-        icon: 'https://ant.design/favicon.ico'
-      },
-      {
-        id: 34,
         title: 'Vue.js',
         url: 'https://vuejs.org/', // 渐进式 JavaScript 框架
         desc: '渐进式 JavaScript 框架，易于上手且功能强大，适合构建用户界面。',
-        icon: 'https://vuejs.org/images/icons/favicon-96x96.png'
+        icon: 'https://vuejs.org/logo.svg'
       },
       {
-        id: 35,
+        id: 32,
         title: 'React',
         url: 'https://reactjs.org/', // 用于构建用户界面的 JavaScript 库
         desc: '用于构建用户界面的 JavaScript 库，由 Facebook 维护，拥有庞大的生态系统。',
         icon: 'https://reactjs.org/favicon.ico'
       },
       {
+        id: 33,
+        title: 'Electron',
+        url: 'https://www.electronjs.org/', // 用于构建跨平台桌面应用的框架
+        desc: '用于构建跨平台桌面应用的框架，结合了 Chromium 和 Node.js 的优势。',
+        icon: 'https://www.electronjs.org/assets/img/favicon.ico'
+      },
+      {
+        id: 34,
+        title: 'Uniapp',
+        url: 'https://uniapp.dcloud.net.cn/',
+        desc: '使用 Vue.js 开发跨平台应用的框架，可以编译到 iOS、Android、Web（微信内置浏览器）和 H5。',
+        icon: 'https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/icon.png?v=1556263038788'
+      },
+      {
+        id: 35,
+        title: 'TDesign UI',
+        url: 'https://tdesign.tencent.com/',
+        desc: '腾讯开源的多端 UI 组件库，提供丰富的组件和设计资源。',
+        icon: 'https://tdesign.tencent.com/favicon.ico'
+      },
+      {
         id: 36,
+        title: 'Ant Design',
+        url: 'https://ant.design/',
+        desc: '蚂蚁金服开源的企业级 UI 设计语言和 React 组件库，广泛应用于中后台产品。',
+        icon: 'https://ant.design/favicon.ico'
+      },
+      {
+        id: 37,
         title: 'element-plus',
         url: 'https://element-plus.org/', // 基于 Vue 3 的桌面端组件库
         desc: '基于 Vue 3 的桌面端组件库，提供丰富的 UI 组件和设计资源。',
         icon: 'https://element-plus.org/images/element-plus-logo-small.svg'
       },
       {
-        id: 37,
-        title: 'Bootstrap',
-        url: 'https://getbootstrap.com/', // 流行的前端框架
-        desc: '流行的前端框架，提供响应式设计和丰富的 UI 组件，帮助快速构建网站和应用。',
-        icon: 'https://getbootstrap.com/docs/5.3/assets/img/favicons/favicon.ico'
-      },
-      {
         id: 38,
-        title: 'Tailwind CSS',
-        url: 'https://tailwindcss.com/', // 实用优先的 CSS 框架
-        desc: '实用优先的 CSS 框架，提供低级别的样式工具，帮助快速构建自定义设计。',
-        icon: 'https://tailwindcss.com/favicon.ico'
-      },
-      {
-        id: 39,
         title: 'elementui',
         url: 'https://element.eleme.io/', // 饿了么开源的 Vue 组件库
         desc: '饿了么开源的 Vue 组件库，提供丰富的 UI 组件和设计资源。',
         icon: 'https://element.eleme.io/favicon.ico'
       },
       {
-        id: 40,
-        title: 'electron',
-        url: 'https://www.electronjs.org/', // 用于构建跨平台桌面应用的框架
-        desc: '用于构建跨平台桌面应用的框架，结合了 Chromium 和 Node.js 的优势。',
-        icon: 'https://www.electronjs.org/images/favicon.ico'
+        id: 39,
+        title: 'Bootstrap',
+        url: 'https://getbootstrap.com/', // 流行的前端框架
+        desc: '流行的前端框架，提供响应式设计和丰富的 UI 组件，帮助快速构建网站和应用。',
+        icon: 'https://getbootstrap.com/docs/5.3/assets/img/favicons/favicon.ico'
+      },
+      {
+        id: 310,
+        title: 'Tailwind CSS',
+        url: 'https://tailwindcss.com/', // 实用优先的 CSS 框架
+        desc: '实用优先的 CSS 框架，提供低级别的样式工具，帮助快速构建自定义设计。',
+        icon: 'https://tailwindcss.com/favicon.ico'
+      },
+      {
+        id: 311,
+        title: 'MDN Web Docs',
+        url: 'https://developer.mozilla.org/', // 前端开发资源
+        desc: '由 Mozilla 维护的前端开发资源，涵盖 HTML、CSS 和 JavaScript 等内容。',
+        icon: 'https://developer.mozilla.org/favicon-48x48.cbbd161b.png'
+      },
+      {
+        id: 312,
+        title: 'Nodejs',
+        url: 'https://nodejs.org',
+        desc: 'Node.js 是一个基于 Chrome V8 引擎的 JavaScript 运行时。',
+        icon: 'https://nodejs.org/static/images/favicons/favicon.png'
+      },
+      {
+        id: 313,
+        title: 'TypeScript中文网',
+        url: 'https://ts.nodejs.cn/',
+        desc: 'TypeScript 是 JavaScript 的一个超集，主要提供了类型注解。',
+        icon: 'https://ts.nodejs.cn/favicon-32x32.png'
+      },
+      {
+        id: 314,
+        title: 'unibest',
+        url: 'https://www.unibest.tech/',
+        desc: '最好的 uniapp 框架, 开箱即用，提供舒适开发体验',
+        icon: 'https://static.unibest.tech/favicon.ico'
       }
     ]
   },
